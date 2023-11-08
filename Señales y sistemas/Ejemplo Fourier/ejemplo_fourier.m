34552b65f7b4626a9280c41a0384f7fb092df357
carmar14 <carmar141414@hotmail.com>
2023-11-08 16:11:41 -0500

    Fourier

---
+++ Señales y sistemas/Ejemplo Fourier/ejemplo_fourier.m
@@ -0,0 +1,85 @@
+clc
+clear
+close all
+
+
+%-------ejemplo señal audio-----
+[y,Fs] = audioread("prueba.mp3");
+y=y(:,1);
+%------vector de tiempo----
+t=0:1/Fs:length(y)*(1/Fs)-(1/Fs);
+t=t';
+plot(t,y)
+
+sound(y,Fs)
+
+%-----calculo de los coeficientes de Fourier
+%Compute the Fourier transform of the signal.
+L=length(y);
+Y = fft(y);
+%Compute the two-sided spectrum P2. Then compute the single-sided spectrum P1 based on P2 and the even-valued signal length L.
+Y_= 2*Y(1:L/2+1);
+P2 = abs(Y/L);
+figure
+plot(2*P2);
+P1 = 2*P2(1:L/2+1);
+
+
+%Define the frequency domain f and plot the single-sided amplitude spectrum P1. 
+f = Fs*(0:(L/2))/L;
+figure
+plot(f,P1,'r')%,f,P3,'k') 
+title('Espectro de amplitud unilateral de y(t)')
+xlabel('f (Hz)')
+ylabel('|Y(f)|')
+%-----calcular fase----
+tol = 1e-6;
+Y_(abs(Y_) < tol) = 0;
+fase=rad2deg(angle(Y_));
+fase=fase(1:L/2+1);
+figure
+plot(f,fase,'r')
+title('Fase de y(t)')
+xlabel('f (Hz)')
+ylabel('arg{Y(f)} °')
+grid on
+%-------ejemplo señal audio-----
+
+%-------ejemplo señal seno-----
+Fs = 1000;            % Sampling frequency                    
+T = 1/Fs;             % Sampling period       
+L = 1500;             % Length of signal
+t = (0:L-1)*T;        % Time vector
+S = 0.7*sin(2*pi*50*t)+ cos(2*pi*20*t+pi/4);
+figure
+plot(t,S)
+
+%-----calculo de los coeficientes de Fourier y su espectro---
+Y = fft(S);
+%Compute the two-sided spectrum P2. Then compute the single-sided spectrum P1 based on P2 and the even-valued signal length L.
+Y_= 2*Y(1:L/2+1);
+P2 = abs(Y/L);
+figure
+plot(2*P2);
+P1 = 2*P2(1:L/2+1);
+
+%Define the frequency domain f and plot the single-sided amplitude spectrum P1. 
+f = Fs*(0:(L/2))/L;
+figure
+plot(f,P1,'r')%,f,P3,'k') 
+title('Espectro de amplitud unilateral de S(t)')
+xlabel('f (Hz)')
+ylabel('|S(f)|')
+grid on
+%-----calcular fase----
+tol = 1e-6;
+Y_(abs(Y_) < tol) = 0;
+fase=rad2deg(angle(Y_));
+
+figure
+plot(f,fase,'r')
+title('Fase de S(t)')
+xlabel('f (Hz)')
+ylabel('arg {S(f)} °')
+grid on
+%-------ejemplo señal seno-----
